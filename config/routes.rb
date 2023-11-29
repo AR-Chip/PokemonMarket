911b30e5137e6e02ba378594a607995b41f6b987
Arjess Reyes <evilares101@hotmail.com>
2023-11-29 17:37:41 -0600

    Implemented Contact and About Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   resources :pokemons, only: %i[index show]
+  get "about", to: "static_pages#about"
+  get "contactus", to: "static_pages#contactus"
 end
