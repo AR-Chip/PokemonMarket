5f5f312779a1803741a594dce8e1f2caf0315905
Arjess Reyes <evilares101@hotmail.com>
2023-11-30 21:43:24 -0600

    Implemented Hierarchical Search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,14 @@
 class SearchController < ApplicationController
   def results
     @query = params[:q]
-    @pokemons = Pokemon.where("name LIKE ?", "%#{@query}%").page(params[:page]).per(20)
+    # @pokemons = Pokemon.where("name LIKE ?", "%#{@query}%").page(params[:page]).per(20)
+
+    # Filter by category if :category parameter is present
+    if params[:type].present?
+      @type = Type.find(params[:type])
+      @pokemons = @type.pokemons.where("name LIKE ?", "%#{@query}%").page(params[:page]).per(20)
+    else
+      @pokemons = Pokemon.where("name LIKE ?", "%#{@query}%").page(params[:page]).per(20)
+    end
   end
 end
