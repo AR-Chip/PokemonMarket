88301eb6028a7193539acd3ea0cc6f213f6a3db5
Arjess Reyes <evilares101@hotmail.com>
2023-12-09 18:04:46 -0600

    Craeted Static Page Model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_06_031202) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_10_000210) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -55,6 +55,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_06_031202) do
     t.integer "quantity"
     t.float "sale_percentage"
     t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "static_pages", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
